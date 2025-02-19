9981a2b03989bab2e3780f72866246ecfd6fbb57
tomtom1307 <tom_ryan1307@icloud.com>
2025-02-19 11:29:22 +0100

    itty bitty initty comitty

---
+++ Assets/Scripts/PlayerController/CameraController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
+    [SerializeField] private float LerpSpeed;
+    [SerializeField] Transform orientation;
+    [SerializeField] Transform HeadPos;
+
+    Camera cam;
+
+    float mousex;
+    float mousey;
+
+    float multiplier = 0.01f;
+
+    float xRot;
+    float yRot;
+
+    private void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        myInput();
+
+        cam.transform.localRotation = Quaternion.Euler(xRot, yRot, 0);
+        orientation.rotation = Quaternion.Euler(0, yRot, 0);
+
+        transform.position = HeadPos.position;
+
+
+    }
+
+
+    void myInput()
+    {
+        mousex = Input.GetAxisRaw("Mouse X");
+        mousey = Input.GetAxisRaw("Mouse Y");
+
+
+        yRot += mousex * sensX * multiplier;
+        xRot -= mousey * sensY * multiplier;
+
+        xRot = Mathf.Clamp(xRot, -90, 90);
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    [SerializeField]
+    private float _moveSpeed, _groundDrag, _airDrag, _airMoveMultiplier, _groundMoveMultiply, _jumpForce;
+    public Transform orientation;
+
+
+    public LayerMask whatisGround;
+
+    float horMovement;
+    float vertMovement;
+
+    float _moveMultiply;
+
+
+    bool isGrounded;
+
+    RaycastHit slopeHit;
+
+    Vector3 slopeMoveDirection;
+    Vector3 MoveDir;
+
+    Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(transform.position+0.3f*Vector3.up, 0.4f, whatisGround);
+
+
+        MyInput();
+        ControlDrag();
+
+        if (OnSlope())
+        {
+            slopeMoveDirection = Vector3.ProjectOnPlane(MoveDir, slopeHit.normal);
+        }
+
+    }
+    void MyInput()
+    {
+        horMovement = Input.GetAxisRaw("Horizontal");
+        vertMovement = Input.GetAxisRaw("Vertical");
+
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        
+        MoveDir = (orientation.forward * vertMovement + orientation.right * horMovement).normalized;
+        if (Vector3.Dot(rb.linearVelocity, MoveDir) < 0) MoveDir = 2 * MoveDir;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void ControlDrag()
+    {
+        if (isGrounded)
+        {
+            rb.linearDamping = _groundDrag;
+            _moveMultiply = _groundMoveMultiply;
+        }
+        else
+        {
+            rb.linearDamping = _airDrag;
+            _moveMultiply = _groundMoveMultiply * _airMoveMultiplier;
+        }
+    }
+    
+    void Jump()
+    {
+        rb.AddForce(_jumpForce * transform.up, ForceMode.Impulse);
+    }
+
+    private bool OnSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, 1.5f))
+        {
+            if(slopeHit.normal != Vector3.up)
+            {
+                rb.useGravity = false;
+                return true;
+                
+            }
+            else
+            {
+                rb.useGravity = true;
+                return false;
+            }
+        }
+        else
+        {
+            rb.useGravity = true;
+            return false;
+        }
+    }
+
+
+
+    void MovePlayer()
+    {
+        if (!OnSlope())
+        {
+            rb.AddForce(MoveDir * _moveSpeed * _moveMultiply, ForceMode.Acceleration);
+        }
+        else if(isGrounded && OnSlope())
+        {
+            rb.AddForce(slopeMoveDirection * _moveSpeed * _moveMultiply, ForceMode.Acceleration);
+        }
+
+    }
+}
