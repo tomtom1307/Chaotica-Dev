ff00e1f981e417780d249b72237105afffc36890
tomtom1307 <tom_ryan1307@icloud.com>
2025-03-05 12:10:27 +0100

    Weapon Swapping!

--- Assets/_Scripts/UpgradeSystems/WeaponUpgrades+Stash/WeaponStash.cs
+++ Assets/_Scripts/UpgradeSystems/WeaponUpgrades+Stash/WeaponStash.cs
@@ -26,27 +26,37 @@ public class WeaponStash : MonoBehaviour
     }
 
     public List<WeaponDataSO> datas;
+    WeaponHolder weaponHolder;
 
     public void DisplayUnlockedWeapons()
     {
 
         PlayerInventory inv = interactable.interactor.GetComponent<PlayerInventory>();
+        weaponHolder = inv.gameObject.GetComponent<WeaponHolder>();
         List<WeaponDataSO> list = inv.weaponStashDatas;
 
         foreach (var weaponData in list)
         {
+            
             if (datas.Contains(weaponData))
             {
                 continue;
             }
+            
             GameObject inst = Instantiate(WeaponItemPrefab, WeaponsContainer);
             datas.Add(weaponData);
             inst.GetComponent<WeaponStashItem>().SetData(weaponData, this);
+            if (weaponHolder.data == weaponData)
+            {
+                currentSelectedWeapon = inst.GetComponent<WeaponStashItem>();
+                Equip();
+            }
         }
 
     }
 
     WeaponStashItem currentSelectedWeapon;
+    [SerializeField] WeaponStashItem currentEquipedWeapon;
 
     public void SelectCurrentWeapon(WeaponStashItem currentWeapon)
     {
@@ -66,5 +76,20 @@ public class WeaponStash : MonoBehaviour
         NameTxt.text = currentSelectedWeapon.data._Name;
         DamageText.text = "Damage: " + currentSelectedWeapon.data.WeaponDamage;
     }
+
+    public void Equip()
+    {
+        Debug.Log("Equip!");
+        if(currentEquipedWeapon != null)
+        {
+            currentEquipedWeapon.SetEquiped(false);
+        }
+        currentEquipedWeapon = currentSelectedWeapon;
+        weaponHolder.SetWeaponData(currentEquipedWeapon.data);
+
+
+        currentEquipedWeapon.SetEquiped(true);
+    }
+
 
 }
