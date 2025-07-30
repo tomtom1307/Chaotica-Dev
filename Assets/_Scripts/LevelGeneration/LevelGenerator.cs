64b70fcf6e9b7ddc368c6b0ca19d1a8cd4c404af
tomtom1307 <tom_ryan1307@icloud.com>
2025-07-30 15:58:58 +0200

    Linear Level Gen

--- Assets/_Scripts/LevelGeneration/LevelGenerator.cs
+++ Assets/_Scripts/LevelGeneration/LevelGenerator.cs
@@ -1,16 +1,37 @@
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float Loops;
+    public float Arenas;
+    public float RoomSpacing;
+    public GameObject Room;
+    public GameObject Store;
+
     void Start()
     {
-        
+        GenerateLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GenerateLevel()
     {
-        
+        //Stores current position
+        Vector3 CurrentPos = Vector3.zero;
+
+
+        for (int l = 0; l < Loops; l++)
+        {
+            // Spawn N number of fight arenas 
+            for(int a = 0;  a < Arenas; a++)
+            {
+                Instantiate(Room, CurrentPos, Quaternion.identity);
+                CurrentPos += RoomSpacing * Vector3.right;
+            }
+
+            //Then Spawn a shop
+            Instantiate(Store, CurrentPos, Quaternion.identity);
+            CurrentPos += RoomSpacing * Vector3.right;
+        }
     }
 }
